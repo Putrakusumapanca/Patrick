091d81dbe66b586b92f7d2e322cb5424cd5673fc
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-25 09:35:35 +0200

    Update ModsMenuState.hx

--- source/ModsMenuState.hx
+++ source/ModsMenuState.hx
@@ -344,6 +344,10 @@ class ModsMenuState extends MusicBeatState
 
 		FlxG.mouse.visible = true;
 
+                #if android
+	        addVirtualPad(UP_DOWN, B);
+                #end
+
 		super.create();
 	}
 
